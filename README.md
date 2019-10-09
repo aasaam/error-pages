e61fb1a89750d85529c5c65d342c72cbf2a23e80
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2019-10-09 20:12:35 +0330

    feat: init

---
+++ README.md
@@ -0,0 +1,54 @@
+# aasaam error pages
+
+[![Greenkeeper badge](https://badges.greenkeeper.io/aasaam/error-pages.svg)](https://greenkeeper.io/)
+
+Simple modified error pages via emoji for web servers that include some processed properties.
+
+## Usage with nginx
+
+Include [error-pages.conf](./dist/nginx/snippets/error-pages.conf) on **http** context and use [server-error-page.conf](./dist/nginx/snippets/server-error-page.conf) **server** context.
+
+## Preview pages
+
+| 3xx | 4xx | 5xx |
+| --- | --- | --- |
+| 🔀 [300 Multiple Choices](./dist/nginx/error-pages/300.html) | 🚫 [400 Bad Request](./dist/nginx/error-pages/400.html) | 💣 [500 Internal Server Error](./dist/nginx/error-pages/500.html) |
+| 🚚 [301 Moved Permanently](./dist/nginx/error-pages/301.html) | 🔐 [401 Unauthorized](./dist/nginx/error-pages/401.html) | 📭 [501 Not Implemented](./dist/nginx/error-pages/501.html) |
+| 🔎 [302 Found](./dist/nginx/error-pages/302.html) | 💰 [402 Payment Required](./dist/nginx/error-pages/402.html) | 🚧 [502 Bad Gateway](./dist/nginx/error-pages/502.html) |
+| 📨 [303 See Other](./dist/nginx/error-pages/303.html) | ⛔ [403 Forbidden](./dist/nginx/error-pages/403.html) | 🚨 [503 Service Unavailable](./dist/nginx/error-pages/503.html) |
+| 💠 [304 Not Modified](./dist/nginx/error-pages/304.html) | ❓ [404 Not Found](./dist/nginx/error-pages/404.html) | ⏲ [504 Gateway Timeout](./dist/nginx/error-pages/504.html) |
+| ℹ️ [307 Temporary Redirect](./dist/nginx/error-pages/307.html) | ❗ [405 Method Not Allowed](./dist/nginx/error-pages/405.html) | 🕯 [505 HTTP Version Not Supported](./dist/nginx/error-pages/505.html) |
+| 🆕 [308 Permanent Redirect](./dist/nginx/error-pages/308.html) | 🛡 [406 Not Acceptable](./dist/nginx/error-pages/406.html) | 💔 [506 Variant Also Negotiates](./dist/nginx/error-pages/506.html) |
+| | 🔩 [407 Proxy Authentication Required](./dist/nginx/error-pages/407.html) | 💯 [507 Insufficient Storage](./dist/nginx/error-pages/507.html) |
+| | ⌛️ [408 Request Timeout](./dist/nginx/error-pages/408.html) | ➰ [508 Loop Detected](./dist/nginx/error-pages/508.html) |
+| | 💥 [409 Conflict](./dist/nginx/error-pages/409.html) | 📈 [509 Bandwidth Limit Exceeded](./dist/nginx/error-pages/509.html) |
+| | 💨 [410 Gone](./dist/nginx/error-pages/410.html) | 🏗 [510 Not Extended](./dist/nginx/error-pages/510.html) |
+| | 📏 [411 Length Required](./dist/nginx/error-pages/411.html) | 🔑 [511 Network Authentication Required](./dist/nginx/error-pages/511.html) |
+| | 🛑 [412 Precondition Failed](./dist/nginx/error-pages/412.html) | |
+| | 🗃 [413 Payload Too Large](./dist/nginx/error-pages/413.html) | |
+| | 🆖 [414 URI Too Long](./dist/nginx/error-pages/414.html) | |
+| | 📼 [415 Unsupported Media Type](./dist/nginx/error-pages/415.html) | |
+| | 📐 [416 Range Not Satisfiable](./dist/nginx/error-pages/416.html) | |
+| | 🤔 [417 Expectation Failed](./dist/nginx/error-pages/417.html) | |
+| | 🍵 [418 I'm a Teapot](./dist/nginx/error-pages/418.html) | |
+| | 🔂 [421 Misdirected Request](./dist/nginx/error-pages/421.html) | |
+| | 💩 [422 Unprocessable Entity](./dist/nginx/error-pages/422.html) | |
+| | 🔒 [423 Locked](./dist/nginx/error-pages/423.html) | |
+| | 😒 [424 Failed Dependency](./dist/nginx/error-pages/424.html) | |
+| | 📤 [426 Upgrade Required](./dist/nginx/error-pages/426.html) | |
+| | ⛓ [428 Precondition Required](./dist/nginx/error-pages/428.html) | |
+| | 🌋 [429 Too Many Requests](./dist/nginx/error-pages/429.html) | |
+| | 🤮 [431 Request Header Fields Too Large](./dist/nginx/error-pages/431.html) | |
+| | 🗑 [444 No Response](./dist/nginx/error-pages/444.html) | |
+| | 🔏️ [451 Unavailable For Legal Reasons](./dist/nginx/error-pages/451.html) | |
+| | 🐳 [494 Request header too large](./dist/nginx/error-pages/494.html) | |
+| | 🏅 [495 SSL Certificate Error](./dist/nginx/error-pages/495.html) | |
+| | 🏷 [496 SSL Certificate Required](./dist/nginx/error-pages/496.html) | |
+| | ❎ [497 HTTP Request Sent to HTTPS Port](./dist/nginx/error-pages/497.html) | |
+
+## Build
+
+```bash
+# build icons
+npm run build:pages
+```
---
+++ bin/build.js
@@ -0,0 +1,142 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// @ts-check
+
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+const httpStatus = require('@aasaam/http-status-extra');
+const {
+  Organization,
+} = require('@aasaam/information');
+const _ = require('lodash');
+const nunjucks = require('nunjucks');
+const sass = require('node-sass');
+const pug = require('pug');
+const cssnano = require('cssnano');
+const stripCssComments = require('strip-css-comments');
+
+(async () => {
+  execSync([
+    `rm -rf ${__dirname}/../tmp`,
+    `rm -rf ${__dirname}/../dist`,
+    `mkdir -p ${__dirname}/../tmp`,
+    `mkdir -p ${__dirname}/../dist/nginx/error-pages`,
+    `mkdir -p ${__dirname}/../dist/nginx/snippets`,
+    `rm -rf ${__dirname}/../tmp/_css_framework.scss`,
+    `cat ${__dirname}/../node_modules/normalize.css/normalize.css > ${__dirname}/../tmp/_css_framework.scss`,
+  ].join(' && '));
+
+  fs.writeFileSync(`${__dirname}/../tmp/_css_framework.scss`, stripCssComments(fs.readFileSync(`${__dirname}/../tmp/_css_framework.scss`, { encoding: 'utf8' }), {
+    preserve: false,
+  }));
+
+  let style = sass.renderSync({
+    file: `${__dirname}/../src/styles/error.scss`,
+    sourceMap: false,
+    outputStyle: 'compressed',
+  }).css.toString('utf-8').trim();
+
+  const nano = await cssnano.process(style, {
+    from: undefined,
+  });
+
+  style = nano.css;
+  const errorCodes = {};
+  Object.keys(httpStatus).forEach((code) => {
+    const c = parseInt(code, 10);
+    // @ts-ignore
+    if (c < 300) {
+      return;
+    }
+    if (httpStatus[c].standard === 'http') {
+      errorCodes[c] = httpStatus[c];
+    }
+
+    if (
+      httpStatus[c].standard === 'nginx'
+      // @ts-ignore
+      && [444, 494, 495, 496, 497].indexOf(c) !== -1
+    ) {
+      errorCodes[c] = httpStatus[c];
+    }
+  });
+
+  const compliedPug = pug.compileFile(`${__dirname}/../src/templates/error.pug`, { pretty: process.env.NODE_ENV === 'test' });
+
+  // generate templates
+  const markDownHelper = {};
+  Object.keys(errorCodes).forEach((c) => {
+    const code = parseInt(c, 10);
+    const { emoji } = errorCodes[code];
+
+    let family = 5;
+    if (code < 400) {
+      family = 3;
+    } else if (code < 500) {
+      family = 4;
+    }
+
+
+    if (!markDownHelper[family]) {
+      markDownHelper[family] = [];
+    }
+    markDownHelper[family].push(`${emoji} [${code} ${errorCodes[code].message}](./dist/nginx/error-pages/${code}.html)`);
+
+    if (!emoji) {
+      throw new Error('Emoji not found');
+    }
+
+    const edgeDebug = {
+      request_id: '%{request_id}',
+      client_uid: '%{client_uid}',
+      user_id: '%{user_id}',
+      agent_name: '%{agent_name}',
+      agent_version: '%{agent_version}',
+      agent_os: '%{agent_os}',
+      agent_os_version: '%{agent_os_version}',
+      agent_category: '%{agent_category}',
+      agent_vendor: '%{agent_vendor}',
+      agent_hash: '%{agent_hash}',
+      geo_country_name: '%{geo_country_name}',
+      geo_country_code: '%{geo_country_code}',
+      geo_city: '%{geo_city}',
+      geo_continent_name: '%{geo_continent_name}',
+      geo_continent_code: '%{geo_continent_code}',
+      geo_timezone: '%{geo_timezone}',
+      geo_latitude: '%{geo_latitude}',
+      geo_longitude: '%{geo_longitude}',
+      geo_accuracy_radius: '%{geo_accuracy_radius}',
+      geo_isp: '%{geo_isp}',
+    };
+
+    const defaultTemplateData = {
+      family,
+      organization: Organization.en,
+      style,
+      title: `${code} ${errorCodes[code].message}`,
+      errorDetail: errorCodes[code],
+      code,
+    };
+
+    const htmlContent = compliedPug(
+      _.merge({}, _.clone(defaultTemplateData), { debug: edgeDebug }),
+    );
+
+    fs.writeFileSync(`${__dirname}/../dist/nginx/error-pages/${code}.html`, htmlContent);
+
+    const errorPages = nunjucks.render(`${__dirname}/../src/templates/nginx/error-pages.j2`, {
+      errorCodes: Object.keys(errorCodes),
+    });
+
+    fs.writeFileSync(`${__dirname}/../dist/nginx/snippets/error-pages.conf`, errorPages.trim());
+
+    const edgeServerLocationError = nunjucks.render(`${__dirname}/../src/templates/nginx/server-error-pages.j2`, {
+      edgeDebug,
+    });
+
+    fs.writeFileSync(`${__dirname}/../dist/nginx/snippets/server-error-page.conf`, edgeServerLocationError.trim());
+  });
+  // console.log(markDownHelper[3].join('\n'));
+  // console.log(markDownHelper[4].join('\n'));
+  // console.log(markDownHelper[5].join('\n'));
+})();
